8daad9cbb951c34f5ee6c155f0009b3f28a9e6d4
kin_river <93558759+kinjo2021@users.noreply.github.com>
2023-09-18 10:04:06 +0000

    Finish chapter5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,9 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+    # => app/views/static_pages/about.html.erb
+  end
+  def contact
+    # => app/views/static_pages/contact.html.erb
   end
 end

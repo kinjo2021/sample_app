de7733dd2d5b7a28a76049fcf5b51ffb4c6b9603
kin_river <93558759+kinjo2021@users.noreply.github.com>
2023-09-24 15:16:20 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root "static_pages#home"
+  # get 'users/new'
   # get  "static_pages/home"
   # get  "static_pages/help"
   # get  "static_pages/about"
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   get  "/signup" , to: "users#new"
+  resources :users
 end

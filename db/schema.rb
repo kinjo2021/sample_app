2dc10d5c64879060c3eeaee20be59ec8a77fa8c2
kin_river <93558759+kinjo2021@users.noreply.github.com>
2023-10-24 13:33:54 +0000

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_113736) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_111948) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
